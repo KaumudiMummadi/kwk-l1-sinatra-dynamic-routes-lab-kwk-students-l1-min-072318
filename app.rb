57b35e9b9bbceb136a9dc8cedac715987070cef0
KaumudiMummadi <kaumudi.mummadi@gmail.com>
2018-07-31 20:17:38 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -28,7 +28,16 @@ end
   
   
  get '/:operation/:number1/:number2'
-  
+ @operation= params[:operation]
+ @number1= params[:number1].to_i
+ @number2= params[:number2].to_i
+    if @operation == "multiply"
+      (@number1 * @number2).to_s
+    elsif @operation == "divide"
+      (@number1/@number2).to_s
+    if @operation == "add"
+      ()
+end
 
     
   
